a585525a5e6d6490153d65aab5202544b6212ec6
Keerti Chaudhary <17keerti@gmail.com>
2022-05-21 19:19:00 -0700

    add reaction Schema

--- models/Thought.js
+++ models/Thought.js
@@ -1,5 +1,32 @@
 const { Schema, model } = require("mongoose");
 
+const reactionSchema = new mongoose.Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      max_length: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
 const thoughtSchema = new Schema(
   {
     thoughtText: {
